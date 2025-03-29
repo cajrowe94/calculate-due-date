16160f625cea2c5f29652fee93f91655fd118f09
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2025-03-29 14:30:20 -0400

    commit to repo

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Caleb Rowe
+ * Emarsys Homework 3-29-25
+ */
+
+function calculateDueDate(submitDateTime = false, turnaroundTime = false) {
+    return '';
+}
+
+function getWorkingHours() {
+    return {
+        // hour, min, sec
+        start: ['09', '00', '00'],
+        end: ['17', '00', '00']
+    }
+}
